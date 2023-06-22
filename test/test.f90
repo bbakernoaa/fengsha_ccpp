8b7a50d7ebe36b223b1281c7e4a9bc529f947fff
zmoon <zmoon92@gmail.com>
2023-06-22 17:26:07 -0600

    Test that produces nonzero emissions

--- test/test.f90
+++ test/test.f90
@@ -12,18 +12,36 @@ program test
    ! Result
    real(rk) :: emissions(5)
 
-   ! Case 1: Full fractions
+   ! Full fractions
    slc = 1
    clay = 1
    sandfrac = 1
    ssm = 1
    rdrag = 1
    airdens = 1.2_rk
+
+   ! uthrs < ustar
    ustar = 0.1_rk
-   uthrs = 1.0_rk
-
+   uthrs = 0.05_rk
    call DustEmissionFENGSHA(slc, clay, sandfrac, ssm, rdrag, airdens, ustar, uthrs, emissions)
-
    print *, emissions
+   call assert(all(emissions > 0), "Emissions exist when uthrs < ustar")
+
+   ! No emissions when uthrs >> ustar
+   uthrs = 1._rk
+   call DustEmissionFENGSHA(slc, clay, sandfrac, ssm, rdrag, airdens, ustar, uthrs, emissions)
+   call assert(all(abs(emissions) <= epsilon(emissions)), "No emissions when uthrs >> ustar")
+
+contains
+
+   subroutine assert(condition, message)
+      logical, intent(in) :: condition
+      character(len=*), intent(in) :: message
+
+      if (.not. condition) then
+         print "('Failed assertion: ', a)", message
+         error stop
+      end if
+   end subroutine assert
 
 end program test
