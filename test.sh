a7ad8a79277c93a84d26e4df995ee7da65e1986f
zmoon <zmoon92@gmail.com>
2023-06-22 15:08:11 -0600

    Update test script

--- test.sh
+++ test.sh
@@ -4,9 +4,5 @@ set -xeuo pipefail
 
 export FC=${FC:-gfortran-10}
 
-# FENGSHA
-make -C src clean
-make -C src
-
-# Wrapper
-$FC -c driver/ccpp/catchem_dust_wrapper.F90 -I./src
+cmake -B build
+cmake --build build
